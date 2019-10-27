ec32ee63f2de251150b3c95da412b652c278678f
Dominik Bittner <DoBi-tyndur@gmx.net>
2019-10-27 18:50:58 +0100

    Add the first unit tests

--- Assignment1/code/src/test/java/de/rwth/swc/group10/OOSCDateTest.java
+++ Assignment1/code/src/test/java/de/rwth/swc/group10/OOSCDateTest.java
@@ -1,5 +1,68 @@
 package de.rwth.swc.group10;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class OOSCDateTest {
-    
+
+    private DateInterface _date;
+
+    @BeforeEach
+    void init() {
+        _date = new OOSCDate();
+    }
+
+    @Test
+    public void constructValidDate() {
+        assertEquals(1, _date.getDay());
+        assertEquals(1, _date.getMonth());
+        assertEquals(1, _date.getYear());
+    }
+
+    @Test
+    public void addDaysMoreThan31() {
+        _date.addDays(40);
+
+        assertEquals(1, _date.getYear());
+        assertEquals(2, _date.getMonth());
+        assertEquals(10, _date.getDay());
+    }
+
+    @Test
+    public void add31Days() {
+        _date.addDays(31);
+
+        assertEquals(1, _date.getYear());
+        assertEquals(2, _date.getMonth());
+        assertEquals(1, _date.getDay());
+    }
+
+    @Test
+    public void addMonthsMoreThan12() {
+        _date.addMonths(13);
+
+        assertEquals(2, _date.getYear());
+        assertEquals(2, _date.getMonth());
+        assertEquals(1, _date.getDay());
+    }
+
+    @Test
+    public void add12Month() {
+        _date.addMonths(12);
+
+        assertEquals(2, _date.getYear());
+        assertEquals(1, _date.getMonth());
+        assertEquals(1, _date.getDay());
+    }
+
+    @Test
+    public void addYears() {
+        _date.addYears(10);
+
+        assertEquals(11, _date.getYear());
+        assertEquals(1, _date.getMonth());
+        assertEquals(1, _date.getDay());
+    }
 }
