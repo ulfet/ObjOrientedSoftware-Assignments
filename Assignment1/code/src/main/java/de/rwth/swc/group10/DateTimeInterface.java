3e4aeabac5340ec14f5a798eb4f7749fa5ed4c7c
Anar Orujov <anar.orujow@gmail.com>
2019-11-01 21:55:12 +0100

    OOSCDateTime completed.

--- Assignment1/code/src/main/java/de/rwth/swc/group10/DateTimeInterface.java
+++ Assignment1/code/src/main/java/de/rwth/swc/group10/DateTimeInterface.java
@@ -1,7 +1,11 @@
 package de.rwth.swc.group10;
 
-public interface OOSCDateTimeInterface extends DateInterface {
+public interface DateTimeInterface extends DateInterface {
 
+	public static final int DATETYPE_HOUR = 3;
+    public static final int DATETYPE_MINUTE = 4;
+    public static final int DATETYPE_SECOND = 5;
+    
 	public void setDateTime(int year, int month, int day, int hour, int minute, int second);
 	public void setTime(int hour, int minute, int second);
     public void setHour(int hour);
@@ -19,4 +23,6 @@ public interface OOSCDateTimeInterface extends DateInterface {
     public void removeSeconds(int secondsToRemove);
     public void removeMinutes(int minutesToRemove);
     public void removeHours(int hoursToRemove);
+    
+    public int timeBetween(int type, DateTimeInterface otherDate);
 }
--- Assignment1/code/src/main/java/de/rwth/swc/group10/OOSCDateTime.java
+++ Assignment1/code/src/main/java/de/rwth/swc/group10/OOSCDateTime.java
@@ -3,7 +3,9 @@ package de.rwth.swc.group10;
 import static org.valid4j.Assertive.ensure;
 import static org.valid4j.Assertive.require;
 
-public class OOSCDateTime extends OOSCDate implements OOSCDateTimeInterface  {
+import java.util.ArrayList;
+
+public class OOSCDateTime extends OOSCDate implements DateTimeInterface, Comparable<DateTimeInterface>  {
 
 	private int _hour;
     private int _minute;
@@ -200,6 +202,110 @@ public class OOSCDateTime extends OOSCDate implements OOSCDateTimeInterface  {
         ensure(invariant(), "The invariant is not valid!");			
 	}
 	
+	@Override
+	public void syncWithUTCTimeserver() {
+        require(invariant(), "inv-v");
+
+        ArrayList<Integer> parts = getCurrentTimeFromUTCTimeServer();
+
+        // Only set new date, if there is a valid result
+        if (parts.size() > 0) {
+            Integer yearReceived = parts.get(0);
+            Integer monthReceived = parts.get(1);
+            Integer dayReceived = parts.get(2);
+            Integer hourReceived = parts.get(3);
+            Integer minuteReceived = parts.get(4);
+            Integer secondReceived = parts.get(5);
+
+            setDateTime(yearReceived, monthReceived, dayReceived, hourReceived, minuteReceived, secondReceived);
+        }
+
+        ensure(invariant(), "The invariant is not valid!");
+    }
+	
+	@Override
+	public int timeBetween(int type, DateTimeInterface otherDateTime) {
+	        require(invariant(), "inv-v");
+	        require(otherDateTime != null, "pre-v: otherDate is NULL");
+	        require(type >=0, "pre-v: The type have to be greater or equals 0");
+	        require(type <= 5, "pre-v: The hour must not be greater 5");
+	        
+	        if(type == DATETYPE_YEAR || type == DATETYPE_MONTH || type == DATETYPE_DAY) {
+	        	return super.daysBetween(otherDateTime);
+	        }	        
+	        
+	        int result = 0;
+	        DateTimeInterface greater;
+            DateTimeInterface smaller;
+            
+	        if(this.compareTo(otherDateTime)>0) {
+	        	greater = this;
+	        	smaller = otherDateTime;
+	        } else {
+	        	greater = otherDateTime;
+	        	smaller = this;
+	        }
+	        
+	        if(type == DATETYPE_HOUR) {
+	        	result = timeBetween(DATETYPE_DAY, otherDateTime)*24 + (greater.getHour() - smaller.getHour());
+	        } else if (type == DATETYPE_MINUTE) {
+	        	result = timeBetween(DATETYPE_HOUR, otherDateTime)*60 + (greater.getMinute() - smaller.getMinute());
+	        } else if (type == DATETYPE_SECOND) {
+	        	result = timeBetween(DATETYPE_MINUTE, otherDateTime)*60 + (greater.getSecond() - smaller.getSecond());
+	        }
+	        ensure(invariant(), "The invariant is not valid!");
+	        return result;
+	    }
+
+	@Override
+	public int compareTo(DateTimeInterface otherDateTime) {
+		require(otherDateTime != null, "pre-v: firstDateTime is NULL");
+		
+		if(this.getYear()>otherDateTime.getYear()) {
+			return 1;
+		} else if (this.getYear()<otherDateTime.getYear()) {
+			return -1;
+		} else if(this.getMonth()>otherDateTime.getMonth()) {
+			return 1;
+		} else if (this.getMonth()<otherDateTime.getMonth()) {
+			return -1;
+		} else if(this.getDay()>otherDateTime.getDay()) {
+			return 1;
+		} else if (this.getDay()<otherDateTime.getDay()) {
+			return -1;
+		} else if(this.getHour()>otherDateTime.getHour()) {
+			return 1;
+		} else if (this.getHour()<otherDateTime.getHour()) {
+			return -1;
+		} else if(this.getMinute()>otherDateTime.getMinute()) {
+			return 1;
+		} else if (this.getMinute()<otherDateTime.getMinute()) {
+			return -1;
+		} else if(this.getSecond()>otherDateTime.getSecond()) {
+			return 1;
+		} else if(this.getSecond()<otherDateTime.getSecond()) {
+			return -1;
+		} 
+		return 0;
+	}
+
+	 
+	@Override
+	public String toString() {
+		
+        require(invariant(), "inv-v");
+        
+        String dateToString = super.toString();
+        String timeToString = String.format(" %02d:%02d:%02d", getHour(), getMinute(), getSecond());
+        String result = dateToString.concat(timeToString);
+        
+        ensure(result != null, "Post-v: Result is null");
+        ensure(result.length() > 0, "Post-v: Result is empty");
+        ensure(invariant(), "The invariant is not valid!");
+
+        return result;
+    }
+	
 	private Boolean invariant() {
         require(_hour >= 0, "pre-v: The hour have to be greater or equals 0");
         require(_hour <= 23, "pre-v: The hour must not be greater 23");
@@ -210,6 +316,5 @@ public class OOSCDateTime extends OOSCDate implements OOSCDateTimeInterface  {
 
         return true;
     }
-
 	
 }
