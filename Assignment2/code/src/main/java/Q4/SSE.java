bce082123b7476fc997bad914fc4bdcd0d282742
Ada Magdalena Slupczynski <ada.slupczynski@rwth-aachen.de>
2019-11-11 17:11:52 +0100

    Added code for Q4, for further dev and revision, not final.

---
+++ Assignment2/code/src/main/java/Q4/SSE.java
@@ -0,0 +1,27 @@
+package Q4;
+
+public class SSE extends Course
+{
+    int grade = 1;
+    String course = "SSE";
+
+    void setGrade(int g)
+    {
+        grade = g;
+    }
+
+    int getGrade()
+    {
+        return grade;
+    }
+
+    void setCourse(String c)
+    {
+        course = c;
+    }
+
+    String getCourse()
+    {
+        return course;
+    }
+}
