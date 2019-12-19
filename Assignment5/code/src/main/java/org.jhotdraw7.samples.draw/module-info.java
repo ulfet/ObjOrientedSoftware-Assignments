3fc66c288d65ff529cad4af7135034e440b27f40
ulfet <mymirrormail@gmail.com>
2019-12-19 14:51:02 +0100

    [FIX]: A5 extra files removed

--- Assignment5/code/src/main/java/org.jhotdraw7.samples.draw/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.jhotdraw7.samples.draw {
-
-    exports org.jhotdraw.samples.draw;
-
-    requires java.desktop;
-    requires org.jhotdraw7.nanoxml;
-    requires org.jhotdraw7.application;
-    requires org.jhotdraw7.draw;
-}
