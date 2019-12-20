37528a0b38bfa425bcb775c5f1150100c4cd4ca5
ulfetcetin <ulfet.rwth@gmail.com>
2019-12-20 08:16:25 +0100

    [FILE]: Maven project created

---
+++ Assignment5/code/ourCode/src/main/java/group10/Main.java
@@ -0,0 +1,14 @@
+package group10;
+
+//import org.jhotdraw.app.Application;
+//import org.jhotdraw.app.OSXApplication;
+//import org.jhotdraw.app.SDIApplication;
+//import org.jhotdraw.util.ResourceBundleUtil;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Alive");
+	}
+
+}
