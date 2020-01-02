f5222f9d46d5d24bea93480e219cc6554524e0ce
Dominik Bittner <DoBi-tyndur@gmx.net>
2020-01-02 14:28:35 +0100

    Comments for the BackgroundImageTool

--- Assignment5/code/jhotdraw/src/main/java/de.rwth.swc.group10/de/rwth/swc/group10/FurnitureOrganizer/tools/BackgroundImageTool.java
+++ Assignment5/code/jhotdraw/src/main/java/de.rwth.swc.group10/de/rwth/swc/group10/FurnitureOrganizer/tools/BackgroundImageTool.java
@@ -8,6 +8,9 @@ import org.jhotdraw.draw.action.MoveAction;
 import org.jhotdraw.draw.tool.ImageTool;
 
 public class BackgroundImageTool extends ImageTool {
+    /**
+     * Move actions, which moves the figure to (0,0)
+     */
     private class ZeroPointMoveAction extends MoveAction {
         public ZeroPointMoveAction(DrawingEditor editor, Figure figure) {
             super(editor, -(int)figure.getStartPoint().getX(), -(int)figure.getStartPoint().getY());
@@ -22,11 +25,15 @@ public class BackgroundImageTool extends ImageTool {
     protected void creationFinished(Figure createdFigure) {
         super.creationFinished(createdFigure);
 
+        // move the created image to (0,0)
         MoveAction ma = new ZeroPointMoveAction(editor, createdFigure);
         ma.actionPerformed(null);
 
+        // Set it as "background"
         getView().getDrawing().sendToBack(createdFigure);
+        // remove it from selection
         getView().removeFromSelection(createdFigure);
+        // and disable selection of this image
         ((ImageFigure) createdFigure).setSelectable(false);
     }
 }
